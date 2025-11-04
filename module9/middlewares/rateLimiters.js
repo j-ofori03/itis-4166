d9d9157d5fefb5fb3fc94edca5b078797b77d22f
Jason Ofori <93500366+j-ofori03@users.noreply.github.com>
2025-11-04 08:28:24 -0500

    Add files via upload

---
+++ module9/middlewares/rateLimiters.js
@@ -0,0 +1,14 @@
+const rateLimit = require('express-rate-limit')
+
+exports.loginLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 min time window
+    max: 5,
+    // message: "Too many login requests. Try again later"
+    handler: (req, res, next) => {
+        console.log('Rate limiter triggered');
+        let err = new Error ('Too many login requests. Try again later');
+        err.status = 429;
+        return next(err);
+    }
+})
+
